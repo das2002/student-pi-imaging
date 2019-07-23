e47d6e2dcd765657daebcb8da6ffdcfcfec7f3fc
Pratyush Das <prat.das2002@gmail.com>
2019-07-23 16:08:09 -0400

    New Time Lapse 7/23

--- time_lapse.py
+++ time_lapse.py
@@ -16,14 +16,16 @@ SESSION_LENGTH = 600# the duration of the script
 camera = PiCamera(resolution = (1280, 720), framerate = 30)
 camera.iso = 100
 sleep(2)
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-g = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = g
+#camera.shutter_speed = camera.exposure_speed
+#camera.exposure_mode = 'off'
+#g = camera.awb_gains
+#camera.awb_mode = 'off'
+#camera.awb_gains = g
 
-camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
-
+#camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
+for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+    print('Captured %s' % filename)
+    sleep(5) # wait 5 seconds
 
 
 # Have the camera take pictures at the specified interval until the session is over
