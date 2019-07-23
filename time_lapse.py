4736ce78d92e4c01f3e0ef11a437ee4949adbf4f
Pratyush Das <prat.das2002@gmail.com>
2019-07-23 15:26:41 -0400

    New Time Lapse Code

--- time_lapse.py
+++ time_lapse.py
@@ -10,15 +10,22 @@ import time
 # Enter the path where the raspberry pi will store its file
 RASPI_PATH = ""
 
-INTERVAL = None# the time interval (in seconds) between pictures
-SESSION_LENGTH = None# the duration of the script
+INTERVAL = 2# the time interval (in seconds) between pictures
+SESSION_LENGTH = 600# the duration of the script
 
-camera = PiCamera()
-camera.start_preview()
+camera = PiCamera(resolution = (1280, 720), framerate = 30)
+camera.iso = 100
+sleep(2)
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
+
+camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
+
 
 
 # Have the camera take pictures at the specified interval until the session is over
 
-
-camera.stop_preview()
 
