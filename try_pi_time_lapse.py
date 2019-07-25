1dd6b85ebe2358957c95324298f6cc2c6285eb26
Pratyush Das <42460747+das2002@users.noreply.github.com>
2019-07-25 09:42:22 -0400

    Create try_pi_time_lapse.py

---
+++ try_pi_time_lapse.py
@@ -0,0 +1,44 @@
+import os
+import datetime
+from datetime import tzinfo
+import math
+from picamera import PiCamera
+from time import sleep
+import time
+import picamera
+import numpy as np
+import cv2
+
+time_at_start = time.time()
+RASPI_PATH = "/home/pi/time_at_start/"
+INTERVAL = 2# the time interval (in seconds) between pictures
+SESSION_LENGTH = 600# the duration of the script
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (320, 240)
+    camera.framerate = 24
+    time.sleep(2)
+    image = np.empty((240 * 320 * 3,), dtype=np.uint8)
+    for filename in camera.capture_continuous(RASPI_PATH + 'img{counter:03d}.bgr'):
+        print('Captured %s' % filename)
+        sleep(INTERVAL) # wait INTERVAL seconds
+    #camera.capture(image, 'bgr')
+        image = image.reshape((240, 320, 3))
+
+# Enter the path where the raspberry pi will store its file
+
+
+#camera = PiCamera(resolution = (1280, 720), framerate = 30)
+#camera.iso = 100
+#sleep(2)
+#camera.shutter_speed = camera.exposure_speed
+#camera.exposure_mode = 'off'
+#g = camera.awb_gains
+#camera.awb_mode = 'off'
+#camera.awb_gains = g
+
+#camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
+
+
+
+# Have the camera take pictures at the specified interval until the session is over
